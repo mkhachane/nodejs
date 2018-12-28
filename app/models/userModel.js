4ac697a47bdf644241dfdbc87f44a38f15e16404
mkhachane <mayur.khachane@opcito.com>
2018-12-28 09:58:49 +0530

    Add email schema into userModel.

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose'), Schema = mongoose.Schema;
 const UserSchema = mongoose.Schema({
       name: String,
       username: String,
+      email: String,
       password: String,
       roles: [{ type: Schema.Types.ObjectId, ref: 'Role' }]
 });
